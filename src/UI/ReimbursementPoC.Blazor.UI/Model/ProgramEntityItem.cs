216de9c4d42c6eb8de8592e5efe3d8ce2b899716
Pavlo <kutsynda@ukr.net>
2023-07-05 16:05:39 +0200

    +

--- src/UI/ReimbursementPoC.Blazor.UI/Model/ProgramEntityItem.cs
+++ src/UI/ReimbursementPoC.Blazor.UI/Model/ProgramEntityItem.cs
@@ -13,14 +13,38 @@
         public DateTime EndDate { get; set; }
     }
 
+    public class UpdateProgramRequest
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public int StateId { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public DateTime LastModified { get; set; }
+    }
+
     public class ProgramEntityItem
     {
         public Guid Id { get; set; }
-        public String Name { get; set; }
-        public String Description { get; set; }
-        public String State { get; set; }
+
+        public string Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public string State { get; set; }
+
         public DateTime StartDate { get; set; }
+
         public DateTime EndDate { get; set; }
+
+        public DateTime LastModified { get; set; }
     }
 
     public class ProgramEntity
