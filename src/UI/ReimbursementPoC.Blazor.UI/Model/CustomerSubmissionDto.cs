21effb5255153ab2507193f8213506c565ed63cb
Pavlo <kutsynda@ukr.net>
2023-07-07 16:03:36 +0200

    +

---
+++ src/UI/ReimbursementPoC.Blazor.UI/Model/CustomerSubmissionDto.cs
@@ -0,0 +1,18 @@
+﻿namespace ReimbursementPoC.Blazor.UI.Model
+{
+    public class CustomerSubmissionDto
+    {
+        public Guid Id { get; set; }
+
+        public Guid CustomerId { get; set; }
+
+        public Guid VendorSubmissionId { get; set; }
+    }
+
+    public class CreateCustomerSubmissionRequest
+    {
+        public Guid CustomerId { get; set; }
+
+        public Guid VendorSubmissionId { get; set; }
+    }
+}
