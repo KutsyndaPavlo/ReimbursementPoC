8c95d79a236048dc225894f21647228dce8a9c92
Pavlo <kutsynda@ukr.net>
2023-07-27 10:27:46 +0200

    +

--- src/ApiGateways/ReimbursementPoC.ApiGateway/Program.cs
+++ src/ApiGateways/ReimbursementPoC.ApiGateway/Program.cs
@@ -2,10 +2,14 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.IdentityModel.Tokens;
 using MMLib.SwaggerForOcelot.DependencyInjection;
+using Ocelot.Authorization;
 using Ocelot.DependencyInjection;
+using Ocelot.DownstreamRouteFinder.UrlMatcher;
 using Ocelot.Middleware;
 using Ocelot.Provider.Polly;
+using Ocelot.Responses;
 using ReimbursementPoC.ApiGateway;
+using System.Security.Claims;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -55,7 +59,7 @@ builder.Configuration.SetBasePath(Directory.GetCurrentDirectory())
     .AddOcelot(routes, builder.Environment)
     .AddEnvironmentVariables();
 
-//builder.Services.DecorateClaimAuthoriser();
+builder.Services.DecorateClaimAuthoriser();
 
 var app = builder.Build();
 
@@ -110,50 +114,50 @@ public class SetGuidMiddleware
     }
 }
 
-//public class ClaimAuthorizerDecorator : IClaimsAuthorizer
-//{
-//    private readonly ClaimsAuthorizer _authoriser;
+public class ClaimAuthorizerDecorator : IClaimsAuthorizer
+{
+    private readonly ClaimsAuthorizer _authoriser;
 
-//    public ClaimAuthorizerDecorator(ClaimsAuthorizer authoriser)
-//    {
-//        _authoriser = authoriser;
-//    }
+    public ClaimAuthorizerDecorator(ClaimsAuthorizer authoriser)
+    {
+        _authoriser = authoriser;
+    }
 
-//    public Response<bool> Authorize(ClaimsPrincipal claimsPrincipal,
-//                                    Dictionary<string, string> routeClaimsRequirement,
-//                                    List<PlaceholderNameAndValue> urlPathPlaceholderNameAndValues)
-//    {
-//        var newRouteClaimsRequirement = new Dictionary<string, string>();
-//        foreach (var kvp in routeClaimsRequirement)
-//        {
-//            if (kvp.Key.StartsWith("http///"))
-//            {
-//                var key = kvp.Key.Replace("http///", "http://");
-//                newRouteClaimsRequirement.Add(key, kvp.Value);
-//            }
-//            else
-//            {
-//                newRouteClaimsRequirement.Add(kvp.Key, kvp.Value);
-//            }
-//        }
+    public Response<bool> Authorize(ClaimsPrincipal claimsPrincipal,
+                                    Dictionary<string, string> routeClaimsRequirement,
+                                    List<PlaceholderNameAndValue> urlPathPlaceholderNameAndValues)
+    {
+        var newRouteClaimsRequirement = new Dictionary<string, string>();
+        foreach (var kvp in routeClaimsRequirement)
+        {
+            if (kvp.Key.StartsWith("http///"))
+            {
+                var key = kvp.Key.Replace("http///", "http://");
+                newRouteClaimsRequirement.Add(key, kvp.Value);
+            }
+            else
+            {
+                newRouteClaimsRequirement.Add(kvp.Key, kvp.Value);
+            }
+        }
 
-//        return _authoriser.Authorize(claimsPrincipal, newRouteClaimsRequirement, urlPathPlaceholderNameAndValues);
-//    }
-//}
+        return _authoriser.Authorize(claimsPrincipal, newRouteClaimsRequirement, urlPathPlaceholderNameAndValues);
+    }
+}
 
-//public static class ServiceCollectionExtensions
-//{
-//    public static IServiceCollection DecorateClaimAuthoriser(this IServiceCollection services)
-//    {
-//        var serviceDescriptor = services.First(x => x.ServiceType == typeof(IClaimsAuthorizer));
-//        services.Remove(serviceDescriptor);
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection DecorateClaimAuthoriser(this IServiceCollection services)
+    {
+        var serviceDescriptor = services.First(x => x.ServiceType == typeof(IClaimsAuthorizer));
+        services.Remove(serviceDescriptor);
 
-//        var newServiceDescriptor = new ServiceDescriptor(serviceDescriptor.ImplementationType, serviceDescriptor.ImplementationType, serviceDescriptor.Lifetime);
-//        services.Add(newServiceDescriptor);
+        var newServiceDescriptor = new ServiceDescriptor(serviceDescriptor.ImplementationType, serviceDescriptor.ImplementationType, serviceDescriptor.Lifetime);
+        services.Add(newServiceDescriptor);
 
-//        services.AddTransient<IClaimsAuthorizer, ClaimAuthorizerDecorator>();
+        services.AddTransient<IClaimsAuthorizer, ClaimAuthorizerDecorator>();
 
-//        return services;
-//    }
-//}
+        return services;
+    }
+}
 
